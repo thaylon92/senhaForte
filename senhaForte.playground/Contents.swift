54c1b6ad6bb22f59969e15132a4b6db19429e8a1
thaylon92 <thaylonmiller@gmail.com>
2023-02-23 22:18:26 -0300

    Algoritimo criado para validar a senha do usuário com os seguinte critérios: - Pelo menos 1 letra maiúscula - Pelo menos 1 letra minúscula - Pelo menos 1 número - A senha deve iniciar com uma letra maiúscula - Não pode conter caracteres especiais - Não pode conter 3 números seguidos em sequência (Ex: **Teste123** - Inválido; **T1es2te3** - Válido) - A senha deve ter entre 5 e 15 caracteres

--- senhaForte.playground/Contents.swift
+++ senhaForte.playground/Contents.swift
@@ -1,3 +1,96 @@
 import UIKit
+import Foundation
+
+
+func checkPassword(_ password: String) -> Bool {
+    // vrifica se possui pelo menos uma letra maisucla
+    
+    let capitalLetterRegEx  = ".*[A-Z]+.*"
+    let texttest = NSPredicate(format:"SELF MATCHES %@", capitalLetterRegEx)
+    guard texttest.evaluate(with: password)  else { return false }
+    
+    // verifica se inicia com uma letra maiuscula
+    
+    guard texttest.evaluate(with: password.prefix(1)) else { return false }
+    
+    // verifica se possui pelo menos uma letra minuscula
+    
+    let lowerLetterRegEx  = ".*[a-z]+.*"
+    let texttest0 = NSPredicate(format:"SELF MATCHES %@", lowerLetterRegEx)
+    guard texttest0.evaluate(with: password) else { return false }
+    
+    // verifica sem possui caracter especial
+    
+    let specialCharacterRegEx: CharacterSet  = [".","*","[","!","&","^","%","$","#","@","(",")","/","]","+",".","*"]
+    let texttest2 = password.rangeOfCharacter(from: specialCharacterRegEx) != nil
+    guard texttest2 == false  else { return false }
+    
+    
+    // verifica se possui pelo menos um numero
+    
+    var numberSet = CharacterSet()
+    numberSet.insert(charactersIn:"0123456789")
+    let texttest1 = password.rangeOfCharacter(from: numberSet ) != nil
+    guard texttest1 == true  else { return false }
+    
+    
+    // verifica se tem numero em sequencia crescente
+    let  numbers = Array(password)
+    var indices: [Int] = []
+    
+    for valor in numbers {
+        if let n = Int(String(valor))  {
+            indices.append(n)
+        } else if indices.count < 3 {
+            indices.removeAll()
+        }
+    }
+    
+    var anterior = -1
+    var isNotIqual = false
+    var verifica: [Bool] = []
+    
+    for x in indices {
+        if anterior == -1 {
+            anterior = x
+        } else {
+            if x == anterior + 1 {
+                isNotIqual = true
+            } else {
+                isNotIqual = false
+            }
+            if isNotIqual == true{
+                verifica.append(true)
+            } else {
+                verifica.append(false)
+            }
+        }
+    }
+    
+    if verifica != [true] {
+        return false
+    } else {
+        
+    }
+    
+    // verificar se tem de 5 a 15 caracter
+    switch password.count {
+    case 5...15:
+        return true
+    default:
+        false
+    }
+    return false
+}
+
+
+
+let pass1 = "Teste123"
+if checkPassword(pass1) {
+    print("Senha forte")
+} else {
+    print("Não atende os requisitos!")
+}
+
 
 
